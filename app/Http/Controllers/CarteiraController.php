88c5399d031bd2005bb88a41a7f2926a8d3b4801
BrunoHenSL <brunosouz16@hotmail.com>
2023-10-22 22:35:00 -0300

    Subindo a api e as rotas
    API Restful criada e rotas definidas para listagem, criação, mostragem e transferência entre carteira

--- app/Http/Controllers/CarteiraController.php
+++ app/Http/Controllers/CarteiraController.php
@@ -41,4 +41,46 @@ class CarteiraController extends Controller
         return response()->json(['message' => 'Carteira criada com sucesso'], 201);
     }
 
+    public function transferir(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'remetente_id' => 'required|exists:carteiras,id',
+            'destinatario_id' => 'required|exists:carteiras,id',
+            'valor' => 'required|numeric|min:0.01',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['message' => 'Dados de transferência inválidos'], 400);
+        }
+
+        $remetente = Carteira::find($request->remetente_id);
+        $destinatario = Carteira::find($request->destinatario_id);
+
+        if (!$remetente || !$destinatario) {
+            return response()->json(['message' => 'Carteira do remetente ou destinatário não encontrada'], 404);
+        }
+
+        // verifica se quem envia tem saldo
+        if ($remetente->saldo < $request->valor) {
+            return response()->json(['message' => 'Saldo insuficiente para realizar a transferência'], 400);
+        }
+
+        DB::beginTransaction();
+
+        try {
+            // atualiza os saldos 
+            $remetente->decrement('saldo', $request->valor);
+            $destinatario->increment('saldo', $request->valor);
+            // aqui confirma a transação
+            DB::commit();
+
+            return response()->json(['message' => 'Transferência realizada com sucesso'], 200);
+        } catch (\Exception $e) {
+            // se não der certo a transação, é retornada a ação
+            DB::rollBack();
+
+            return response()->json(['message' => 'Falha na transferência'], 500);
+        }
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/carteira', 'CarteiraController@listarCarteiras');
+Route::get('/carteira/{id}', 'CarteiraController@mostrarSaldo');
+Route::post('/carteira', 'CarteiraController@criarCarteira');
+Route::post('/carteira/transferir', 'CarteiraController@transferir');
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
